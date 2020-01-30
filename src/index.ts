561a3a6bad135a94d08d276199e7daaf40d75d1a
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-01-31 00:54:45 +0900

    BREAKING CHANGE: simplify API

--- src/index.ts
+++ src/index.ts
@@ -10,63 +10,66 @@ export interface AssetMetaData {
     folderAsset: 'yes' | 'no' | undefined
 }
 
-export const loadAssetMetaData = (data: string): AssetMetaData => safeLoad(data) as AssetMetaData;
-
-function DoNothing(err: NodeJS.ErrnoException | null) { if (err) throw err; };
-
-export const createUnityPackageFromFolder = (folderContainsMetaFolders: string, output: string, callback?: NoParamCallback) => {
-    const archtemp = join(tmpdir(), "archtemp.tar");
-    execSync('tar -cf ' + archtemp + ' -C "' + folderContainsMetaFolders + '" ./');
-    readFile(archtemp, (err, data) => {
-        if (err) throw err;
-        gzip(data, (err, data) => {
+export module UnityPackageCreator {
+    export const loadAssetMetaData = (data: string): AssetMetaData => safeLoad(data) as AssetMetaData;
+    function DoNothing(err: NodeJS.ErrnoException | null) { if (err) throw err; };
+    export const createUnityPackageFromFolder = (folderContainsMetaFolders: string, output: string, callback?: NoParamCallback, logger?: (logText: string) => void) => {
+        const archtemp = join(tmpdir(), "archtemp.tar");
+        execSync('tar -cf ' + archtemp + ' -C "' + folderContainsMetaFolders + '" ./');
+        readFile(archtemp, (err, data) => {
             if (err) throw err;
-            writeFile(output, data, () => rmdir(folderContainsMetaFolders, callback || DoNothing));
-        });
-    });
-};
-
-export const createMetaFolderUnderFolder = (folderContainsMetaFolders: string, metaFileRelativePathWithExtension: string, projectRoot: string, callback?: NoParamCallback) => {
-    const metaFileAbsolutePath = join(projectRoot, metaFileRelativePathWithExtension);
-    readFile(metaFileAbsolutePath, { encoding: "utf-8" }, async (err, data) => {
-        if (err) throw err;
-        const metaDatum = loadAssetMetaData(data);
-        const guid = metaDatum.guid;
-        const dir = join(folderContainsMetaFolders, guid);
-
-        mkdir(dir, () => {
-            copyFile(metaFileAbsolutePath, join(dir, "asset.meta"), () => {
-                if (metaDatum.folderAsset !== "yes") {
-                    const assetFileAbsolutePath = metaFileAbsolutePath.substr(0, metaFileAbsolutePath.length - 5);
-                    copyFileSync(assetFileAbsolutePath, join(dir, "asset"));
-                }
-
-                const assetFileRelativePath = metaFileRelativePathWithExtension.substr(0, metaFileRelativePathWithExtension.length - 5);
-                writeFile(join(dir, "pathname"), assetFileRelativePath, callback || DoNothing);
+            gzip(data, (err, data) => {
+                if (err) throw err;
+                if (logger)
+                    logger('write-done : ' + output);
+                writeFile(output, data, () => rmdir(folderContainsMetaFolders, callback || DoNothing));
             });
         });
-    });
-};
+    };
 
-const createUnityPackageFromMetaFilePathsWithTempFolder = (metaFiles: string[], projectRoot: string, output: string, folderContainsMetaFolders: string) => {
-    const processHasDone = new Array(metaFiles.length);
-    processHasDone.fill(false);
-    metaFiles.forEach((metaFilePath, index, _) => {
-        const callback = () => {
-            processHasDone[index] = true;
-            if (processHasDone.indexOf(false) === -1)
-                createUnityPackageFromFolder(folderContainsMetaFolders, output);
-        };
-        createMetaFolderUnderFolder(metaFilePath, projectRoot, folderContainsMetaFolders, callback);
-    });
-};
+    export const createMetaFolderUnderFolder = (folderContainsMetaFolders: string, metaFileRelativePathWithExtension: string, projectRoot: string, callback?: NoParamCallback, logger?: (logText: string) => void) => {
+        const metaFileAbsolutePath = join(projectRoot, metaFileRelativePathWithExtension);
+        readFile(metaFileAbsolutePath, { encoding: "utf-8" }, async (err, data) => {
+            if (err) throw err;
+            const metaDatum = loadAssetMetaData(data);
+            const guid = metaDatum.guid;
+            const dir = join(folderContainsMetaFolders, guid);
 
-export const createUnityPackageFromMetaFilePaths = (metaFiles: string[], projectRoot: string, output: string) => {
+            if (logger)
+                logger('create-directory : ' + dir);
+            mkdir(dir, () => {
+                copyFile(metaFileAbsolutePath, join(dir, "asset.meta"), () => {
+                    if (metaDatum.folderAsset !== "yes") {
+                        const assetFileAbsolutePath = metaFileAbsolutePath.substr(0, metaFileAbsolutePath.length - 5);
+                        copyFileSync(assetFileAbsolutePath, join(dir, "asset"));
+                    }
+
+                    const assetFileRelativePath = metaFileRelativePathWithExtension.substr(0, metaFileRelativePathWithExtension.length - 5);
+                    writeFile(join(dir, "pathname"), assetFileRelativePath, callback || DoNothing);
+                });
+            });
+        });
+    };
+    export const createUnityPackageFromMetaFilePathsWithTempFolder = (metaFiles: string[], projectRoot: string, output: string, folderContainsMetaFolders: string, logger?: (logText: string) => void) => {
+        const processHasDone = new Array(metaFiles.length);
+        processHasDone.fill(false);
+        metaFiles.forEach((metaFilePath, index, _) => {
+            const callback = () => {
+                processHasDone[index] = true;
+                if (processHasDone.indexOf(false) === -1)
+                    createUnityPackageFromFolder(folderContainsMetaFolders, output);
+            };
+            createMetaFolderUnderFolder(metaFilePath, projectRoot, folderContainsMetaFolders, callback, logger);
+        });
+    };
+}
+
+export const createUnityPackage = (metaFiles: string[], projectRoot: string, output: string, logger?: (logText: string) => void) => {
     mkdtemp("tempFolder", (err, folder) => {
         if (err) throw err;
         const folderContainsMetaFolders = join(folder, 'archtemp');
         mkdir(folderContainsMetaFolders, () => {
-            createUnityPackageFromMetaFilePathsWithTempFolder(metaFiles, projectRoot, output, folderContainsMetaFolders);
+            UnityPackageCreator.createUnityPackageFromMetaFilePathsWithTempFolder(metaFiles, projectRoot, output, folderContainsMetaFolders, logger);
         });
     });
 };
